4f2274140e93ece4995bb7935e9332308e450245
Eugenia Kim <ek2213@ic.ac.uk>
2017-02-15 18:20:38 +0000

    Reliabiltiy var extracted and test cases added

--- system6/process.erl
+++ system6/process.erl
@@ -1,12 +1,11 @@
 % Eugenia Kim (ek2213) and Radu-Andrei Szasz (ras114)
 
 -module(process).
--export([start/2]).
+-export([start/3]).
 
-start(System, IntId) ->
+start(System, IntId, Reliability) ->
   Id = list_to_atom(integer_to_list(IntId)),
-  PL = spawn(pl, start, [System, Id, 100]),
-  BEB = spawn(beb, start, [PL]),
+  PL = spawn(pl, start, [System, Id, Reliability]), BEB = spawn(beb, start, [PL]),
   RB = spawn(rb, start, [BEB, Id]),
   App = spawn(app, start, [RB, Id]),
   RB ! {bind_upper_layer, App},
