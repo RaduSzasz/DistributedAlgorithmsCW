3e662794192be94e9c2fab0de09bbe1c7f9aab05
Radu Szasz <ras114@ic.ac.uk>
2017-02-15 18:26:01 +0000

    Add comments to system2

--- system2/system2.erl
+++ system2/system2.erl
@@ -13,6 +13,19 @@
 % '2': "{70180, 10792} {70180, 21637} {70180, 12827} {70180, 21141} {70180, 19523}"
 % '1': "{100683, 5447} {100683, 11916} {100683, 7694} {100683, 5332} {100683, 5353}"
 % '4': "{52668, 14635} {52668, 32011} {52668, 18627} {52668, 38296} {52668, 32460}"
+%
+% For the first case, the output is very similar to the one
+% we had in system1.
+%
+% However, for the set with unbounded message sending we notice that the
+% amount of messages sent is much greater than the amount of messages
+% received. This is due to the way we implemented the pl process.
+% In its receive block we are firstly processing the send requests that
+% are coming from the layer above (in this case the App) before we are
+% processing the messages that arrived from other processes. Therefore,
+% the much bigger difference in between sent and received follows naturally.
+%
+% This behaviour will be observed throughout all of the next systems.
 -module(system2).
 -export([start/0]).
 
