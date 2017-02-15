2f23010991d836bd98f8b75c8f8d57139b1bf2f0
Radu Szasz <ras114@ic.ac.uk>
2017-02-15 18:47:35 +0000

    Comment system5

--- system5/system5.erl
+++ system5/system5.erl
@@ -13,6 +13,16 @@
 % '5': "{197713, 726} {197713, 323} {197713, 110} {197713, 871} {197713, 327}"
 % '4': "{171024, 3483} {171024, 2764} {171024, 1426} {171024, 3148} {171024, 2370}"
 % '2': "{206369, 2042} {206369, 1273} {206369, 518} {206369, 2289} {206369, 1095}"
+%
+% As expected, in both cases process 3 finished first, as its timeout was significantly
+% smaller than the timeout for the other processes.
+%
+% It is interesting to notice that in the bounded case, process 3 managed to both
+% send and received all the messages it was expected to. However, in the unbounded
+% case, it only managed to send messages, but not receive any. This is because of
+% the fact that was constantly repeated in the comments of the other systems. Since
+% sendind messages has priority over receiving messages, this caused process 3 to not
+% have time to receive any message in the 5 milliseconds it was active for.
 -module(system5).
 -export([start/0]).
 
