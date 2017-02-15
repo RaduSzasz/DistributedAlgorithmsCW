0c3277268cf08e8cee51a7e1577593373e411eae
Radu Szasz <ras114@ic.ac.uk>
2017-02-15 18:51:33 +0000

    Commented system6

--- system6/system6.erl
+++ system6/system6.erl
@@ -1,19 +1,5 @@
 % Eugenia Kim (ek2213) and Radu-Andrei Szasz (ras114)
 %
-% Output for Max_messages = 100, Timeout = 1000, Reliability = 100, Short_timeout = 5:
-% '3': "{100, 100} {100, 100} {100, 100} {100, 100} {100, 100}"
-% '2': "{100, 100} {100, 100} {100, 100} {100, 100} {100, 100}"
-% '1': "{100, 100} {100, 100} {100, 100} {100, 100} {100, 100}"
-% '5': "{100, 100} {100, 100} {100, 100} {100, 100} {100, 100}"
-% '4': "{100, 100} {100, 100} {100, 100} {100, 100} {100, 100}"
-%
-% Output for Max_messages = 0, Timeout = 1000, Reliability = 100, Short_timeout = 5:
-% '3': "{1065, 0} {1065, 0} {1065, 0} {1065, 0} {1065, 0}"
-% '1': "{381591, 573} {381591, 255} {381591, 93} {381591, 703} {381591, 255}"
-% '5': "{197713, 726} {197713, 323} {197713, 110} {197713, 871} {197713, 327}"
-% '4': "{171024, 3483} {171024, 2764} {171024, 1426} {171024, 3148} {171024, 2370}"
-% '2': "{206369, 2042} {206369, 1273} {206369, 518} {206369, 2289} {206369, 1095}"
-%
 % Output for Max_messages = 100, Timeout = 3000, Reliability = 100, Short_timeout = 5:
 %'3': "{100, 1} {100, 1} {100, 1} {100, 1} {100, 1}"
 %'1': "{100, 1} {100, 1} {100, 1} {100, 1} {100, 1}"
@@ -35,6 +21,11 @@
 % '5': "{106447, 1} {106447, 1} {106447, 1} {106447, 1} {106447, 1}"
 % '3': "{527410, 1} {527410, 1} {527410, 1} {527410, 1} {527410, 1}"
 %
+% It is interesting to observe that for all the processes the number of responses received
+% was 1. This is due to the filtering that the Reliable Broadcast imposed on our messages.
+% We could have went past this limitation by adding some payload to the messages in order to
+% identify that they are different. However, we considered that it would better illustrate the
+% point and it would bring not much extra doing otherwise.
 -module(system6).
 -export([start/0]).
 
